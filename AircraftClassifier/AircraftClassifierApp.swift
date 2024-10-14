ee1346b3a85015ba4032a64337cb683969cc264a
Doug Holland <doug.holland@outlook.com>
2024-10-14 10:50:29 -0700

    Initial Commit

---
+++ AircraftClassifier/AircraftClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AircraftClassifierApp.swift
+//  AircraftClassifier
+//
+//  Created by Doug Holland on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AircraftClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
